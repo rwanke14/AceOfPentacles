2f0fff6c094047bcf633d35429781c18a78532e3
johnniesimpson <johnniesimpson@outlook.com>
2020-12-17 20:08:53 -0500

    Added Astrology API to JS file

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Hello world');
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://astrology-horoscope.p.rapidapi.com/zodiac_finder/details_requirements/",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "87b6f48f43mshd9aaace1d8eeaefp1a1912jsn1259fae457e2",
+		"x-rapidapi-host": "astrology-horoscope.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
